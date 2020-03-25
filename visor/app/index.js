367a59fd7c4a41d2bdf214d23cc7ed23671d775d
equipo <equipo@LAPTOP-6TJ4BQ81>
2020-03-25 13:46:35 -0500

    Interaccion Departamento y Municipio

--- visor/app/index.js
+++ visor/app/index.js
@@ -1,6 +1,11 @@
 //Evento cargue de pagina principal lista
 $(document).ready(function () {
     //
+    //Importar scripts adicionales
+    //
+    importarScript("app/layers.js");
+    importarScript("app/jsSearch.js");
+    //
     //Agragar elementos principales DOM
     //
     //lista departamento
@@ -8,32 +13,142 @@ $(document).ready(function () {
     dropdown.className = 'dropdown';
     var button = document.createElement('button');
     button.type = 'button';
+    button.id = 'BuListDepto';
     button.className = 'btn btn-primary dropdown-toggle';
     button.setAttribute('data-toggle', 'dropdown');
+    button.onclick = AgregarListDepto;
     button.innerHTML = 'Seleccione Departamento';
     dropdown.appendChild(button);
-    document.querySelector('body').appendChild(dropdown);
+
     var dropdownMenu = document.createElement('div');
-    dropdownMenu.className = 'dropdown-menu';
+    dropdownMenu.className = 'dropdown-menu scrollable-menu';
+    dropdownMenu.id = 'ListDepto';
     dropdown.appendChild(dropdownMenu);
-    
-    var a = document.createElement('a');
-    a.className = 'dropdown-item';
-    a.innerHTML = 'Amazonas';
-    dropdownMenu.appendChild(a);
-    
+
+    var button1 = document.createElement('button');
+    button1.type = 'button';
+    button1.id = 'BuListMpio';
+    button1.className = 'btn btn-primary dropdown-toggle';
+    button1.setAttribute('data-toggle', 'dropdown');
+    button1.onclick = AgregarListMpio;
+    button1.innerHTML = 'Seleccione Municipio';
+    dropdown.appendChild(button1);
+    document.querySelector('body').appendChild(dropdown);
+
     //Mapa
-    var map = document.createElement('div');
-    map.id = 'map';
-    document.querySelector('body').appendChild(map);
-    //
-    //Importar scripts adicionales
-    //
-    importarScript("app/layers.js");
+    var mapd = document.createElement('div');
+    mapd.id = 'map';
+    document.querySelector('body').appendChild(mapd);
+    $(".dropdown-menu").on("click", "a", function (event) {
+        if (document.getElementById('BuListDepto').innerHTML === 'Seleccione Departamento' || document.getElementById('BuListMpio').innerHTML !== 'Seleccione Municipio') {
+            var viewParamsStr = viewparamsToStr({
+                query: event.target.text
+            });
+            var tempname = "Visor:SelGeomDepto";
+        } else {
+            var viewParamsStr = viewparamsToStr({
+                query: event.target.text,
+                query1: document.getElementById('BuListDepto').innerHTML
+            });
+            var tempname = "Visor:SelGeomMpio";
+        }
+        var url = 'http://34.231.25.67:8080/geoserver/ows?';
+        var wfsParams = {
+            service: 'WFS',
+            version: '2.0.0',
+            request: 'GetFeature',
+            typeName: tempname,
+            outputFormat: 'application/json',
+            srsname: 'EPSG:3857',
+            viewparams: viewParamsStr
+        };
+        $.ajax({
+            url: url,
+            data: wfsParams,
+            type: "GET",
+            dataType: "json",
+            success: function (data) {
+                var vectorSource = new ol.source.Vector({
+                    features: (new ol.format.GeoJSON()).readFeatures(data)
+                });
+                highlight.getSource().clear();
+                highlight.getSource().addFeature(vectorSource.getFeatures()[0]);
+                var view = map.getView()
+                view.fit(vectorSource.getExtent());
+            }
+        });
+        if (tempname === 'Visor:SelGeomDepto') {
+            document.getElementById('BuListDepto').innerHTML = event.target.text;
+            var filtro = '"nombre_dpt=' + "'" + event.target.text + "'" + '"';
+            Mpios.getSource().updateParams({'STYLES': 'Mpio', 'CQL_FILTER': eval(filtro)});
+            Mpios.setVisible(true);
+            document.getElementById('BuListMpio').innerHTML = 'Seleccione Municipio';
+            document.getElementById('BuListMpio').disabled = false;
+        } else {
+            document.getElementById('BuListMpio').innerHTML = event.target.text;
+            document.getElementById('BuListMpio').disabled = true;
+        }
+    });
 });
 function importarScript(nombre, callback) {
     var s = document.createElement("script");
     s.onload = callback;
     s.src = nombre;
     document.querySelector("head").appendChild(s);
+}
+function AgregarListDepto() {
+    var obj = document.getElementById("ListDepto");
+    if (document.getElementById('BuListDepto').innerHTML === 'Seleccione Departamento') {
+        var depto = buscar("Visor:SelNomDepto");
+        for (i = 0; i < depto.length; i++) {
+            var a = document.createElement('a');
+            a.className = 'dropdown-item';
+            a.setAttribute('value', depto[i][0]);
+            a.innerHTML = depto[i][0];
+            obj.appendChild(a);
+        }
+    } else if (document.getElementById('BuListDepto').innerHTML !== 'Seleccione Municipio') {
+        var obj1 = document.getElementById("ListDepto");
+        var len = obj1.getElementsByTagName('a').length;
+        for (i = 0; i < len; i++) {
+            obj1.removeChild(obj1.getElementsByTagName('a')[0]);
+        }
+        var depto = buscar("Visor:SelNomDepto");
+        for (i = 0; i < depto.length; i++) {
+            var a = document.createElement('a');
+            a.className = 'dropdown-item';
+            a.setAttribute('value', depto[i][0]);
+            a.innerHTML = depto[i][0];
+            obj.appendChild(a);
+        }
+    }
+}
+function AgregarListMpio() {
+    var obj = document.getElementById("ListDepto");
+    if (document.getElementById('BuListDepto').innerHTML === 'Seleccione Departamento') {
+        alert("Primero seleccione un departamento");
+    } else {
+        var obj1 = document.getElementById("ListDepto");
+        var len = obj1.getElementsByTagName('a').length;
+        for (i = 0; i < len; i++) {
+            obj1.removeChild(obj1.getElementsByTagName('a')[0]);
+        }
+        var mpio = buscar("Visor:SelNomMpio", document.getElementById('BuListDepto').innerHTML);
+        for (i = 0; i < mpio.length; i++) {
+            var a = document.createElement('a');
+            a.className = 'dropdown-item';
+            a.setAttribute('value', mpio[i][0]);
+            a.innerHTML = mpio[i][0];
+            obj1.appendChild(a);
+        }
+    }
+}
+//dropdown-item
+function viewparamsToStr(obj) {
+    var str = '';
+    $.each(obj, function (k, v) {
+        str.length && (str += ';');
+        str += k + ':' + v;
+    });
+    return str;
 }
--- visor/app/layers.js
+++ visor/app/layers.js
@@ -7,12 +7,11 @@ var streetmap = new ol.layer.Tile({
     minResolution: 2,
     name: 'Street Map'
 });
-
 var highlight = new ol.layer.Vector({
     style: new ol.style.Style({
         stroke: new ol.style.Stroke({
             color: '#00FFFF',
-            width: 3
+            width: 5
         })
     }),
     source: new ol.source.Vector()
@@ -22,14 +21,21 @@ var Deptos = new ol.layer.Tile({
     visible: true,
     source: new ol.source.TileWMS({
         url: 'http://34.231.25.67:8080/geoserver/Visor/wms',
-        params: {LAYERS: 'Visor:departamentos', STYLES: ''}
+        params: {LAYERS: 'Visor:departamento', STYLES: ''}
     }), name: 'Departamentos'
+});
+var Mpios = new ol.layer.Tile({
+    visible: false,
+    source: new ol.source.TileWMS({
+        url: 'http://34.231.25.67:8080/geoserver/Visor/wms',
+        params: {LAYERS: 'Visor:municipio', STYLES: ''}
+    }), name: 'Municipios'
 });
 //
 //Grupos de capas
 //
 var layerBase = new ol.layer.Group({
-    layers: [streetmap, highlight],
+    layers: [streetmap/*, highlight*/],
     name: 'Capas Base'
 });
 //
@@ -46,108 +52,108 @@ map = new ol.Map({
     target: document.getElementById('map'),
     // use the Canvas renderer
     renderer: 'canvas',
-    layers: [layerBase, Deptos/*new ol.layer.Tile({ source: new ol.source.OSM()})/*, highlight*/],
+    layers: [layerBase, Deptos, Mpios, highlight],
     view: new ol.View({
         center: [-8113332, 464737],
         zoom: 5.373
     })
 });
-function buildLayerTree(layer) {
-    console.log(layer);
-    console.log(layer.getLayers);
-    var elem;
-    var name = layer.get('name') ? layer.get('name') : "Group";
-    //console.log(name);
-    //if (name !== 'Group') {
-        //console.log(1);
-        if (layer.values_.visible === true && layer.get('name') !== 'CAPAS') {
-            console.log(name);
-            var div = "<li data-layerid='" + name + "'>" + "<span><img src='iconos/" + layer.get('name') + ".jpg' alt='Smiley face' height='20' width='20' >" + layer.get('name') + "</span>" + "<i class='glyphicon glyphicon-check'></i> ";
-        } else if (layer.get('name') === 'CAPAS') {
-            console.log(3);
-            var div = "<li data-layerid='" + name + "'>" + "<span><img src='image/" + layer.get('name') + ".png' alt='Smiley face' height='30' width='100'>" + layer.get('name') + "</span>";
-        } else {
-            console.log(4);
-            var div = "<li data-layerid='" + name + "'>" + "<span><img src='image/" + layer.get('name') + ".jpg' alt='Smiley face' height='20' width='20'>" + layer.get('name') + "</span>" + "<i class='glyphicon glyphicon-unchecked'></i> ";
-        }
-        if (layer.getLayers) {
-            
-            var sublayersElem = '';
-            var layers = layer.getLayers().getArray();
-            console.log(layers);
-            var len = layers.length;
-            for (var i = len - 2; i >= 0; i--) {
-                console.log("for");
-                sublayersElem += buildLayerTree(layers[i]);
-            }
-            elem = div + " <ul>" + sublayersElem + "</ul></li>";
-        } else {
-            elem = div + " </li>";
-        }
-        return elem;
-    //}
-}
-function initializeTree() {
-    //console.log("inicio tree");
-    //console.log(ol.map);
-    //console.log(map);
-    var elem = buildLayerTree(map.getLayerGroup());
-    console.log(elem);
-    $('#layertree').empty().append(elem);
-    $('.tree li:has(ul)').addClass('parent_li').find(' > span').attr('title', 'Collapse this branch');
-    var longitud = $('.tree li:has(ul)').addClass('parent_li').find(' > span').attr('title', 'Collapse this branch').length;
-    for (var i = 0; i < longitud; i++) {
-        var colapse = $('.tree li:has(ul)').addClass('parent_li').find(' > span').attr('title', 'Collapse this branch')[i];
-        var colap = $(colapse).parent('li.parent_li').find(' > ul > li');
-        colap.hide('fast');
-    }
-    $('.tree li.parent_li > span').on('click', function (e) {
-        var children = $(this).parent('li.parent_li').find(' > ul > li');
-        if (children.is(":visible")) {
-            children.hide('fast');
-            $(this).attr('title', 'Expand this branch').find(' > i').addClass('glyphicon-plus').removeClass('glyphicon-minus');
-        } else {
-            children.show('fast');
-            $(this).attr('title', 'Collapse this branch').find(' > i').addClass('glyphicon-minus').removeClass('glyphicon-plus');
-        }
-        e.stopPropagation();
-    });
-}
-function findBy(layer, key, value) {
-    if (layer.get(key) === value) {
-        return layer;
-    }
-    // Find recursively if it is a group
-    if (layer.getLayers) {
-        var layers = layer.getLayers().getArray(),
-                len = layers.length, result;
-        for (var i = 0; i < len; i++) {
-            result = findBy(layers[i], key, value);
-            if (result) {
-                return result;
-            }
-        }
-    }
-    return null;
-}
-$(document).ready(function () {
-    //console.log("inicio");
-    initializeTree();
-    // Handle opacity slider control
-    $('input.opacity').slideDown().on('slide', function (ev) {
-        var layername = $(this).closest('li').data('layerid');
-        var layer = findBy(map.getLayerGroup(), 'name', layername);
-        layer.setOpacity(ev.value);
-    });
-    // Handle visibility control
-    $('i').on('click', function () {
-        var layername = $(this).closest('li').data('layerid');
-        var layer = findBy(map.getLayerGroup(), 'name', layername);
-        layer.setVisible(!layer.getVisible());
-        if (layer.getVisible()) {
-            $(this).removeClass('glyphicon-unchecked').addClass('glyphicon-check');
-        } else {
-            $(this).removeClass('glyphicon-check').addClass('glyphicon-unchecked');
-        }
-    });
-});
+/*function buildLayerTree(layer) {
+ //console.log(layer);
+ //console.log(layer.getLayers);
+ var elem;
+ var name = layer.get('name') ? layer.get('name') : "Group";
+ //console.log(name);
+ //if (name !== 'Group') {
+ //console.log(1);
+ if (layer.values_.visible === true && layer.get('name') !== 'CAPAS') {
+ //console.log(name);
+ var div = "<li data-layerid='" + name + "'>" + "<span><img src='iconos/" + layer.get('name') + ".jpg' alt='Smiley face' height='20' width='20' >" + layer.get('name') + "</span>" + "<i class='glyphicon glyphicon-check'></i> ";
+ } else if (layer.get('name') === 'CAPAS') {
+ //console.log(3);
+ var div = "<li data-layerid='" + name + "'>" + "<span><img src='image/" + layer.get('name') + ".png' alt='Smiley face' height='30' width='100'>" + layer.get('name') + "</span>";
+ } else {
+ //console.log(4);
+ var div = "<li data-layerid='" + name + "'>" + "<span><img src='image/" + layer.get('name') + ".jpg' alt='Smiley face' height='20' width='20'>" + layer.get('name') + "</span>" + "<i class='glyphicon glyphicon-unchecked'></i> ";
+ }
+ if (layer.getLayers) {
+ 
+ var sublayersElem = '';
+ var layers = layer.getLayers().getArray();
+ //console.log(layers);
+ var len = layers.length;
+ for (var i = len - 2; i >= 0; i--) {
+ //console.log("for");
+ sublayersElem += buildLayerTree(layers[i]);
+ }
+ elem = div + " <ul>" + sublayersElem + "</ul></li>";
+ } else {
+ elem = div + " </li>";
+ }
+ return elem;
+ //}
+ }
+ function initializeTree() {
+ //console.log("inicio tree");
+ //console.log(ol.map);
+ //console.log(map);
+ var elem = buildLayerTree(map.getLayerGroup());
+ //console.log(elem);
+ $('#layertree').empty().append(elem);
+ $('.tree li:has(ul)').addClass('parent_li').find(' > span').attr('title', 'Collapse this branch');
+ var longitud = $('.tree li:has(ul)').addClass('parent_li').find(' > span').attr('title', 'Collapse this branch').length;
+ for (var i = 0; i < longitud; i++) {
+ var colapse = $('.tree li:has(ul)').addClass('parent_li').find(' > span').attr('title', 'Collapse this branch')[i];
+ var colap = $(colapse).parent('li.parent_li').find(' > ul > li');
+ colap.hide('fast');
+ }
+ $('.tree li.parent_li > span').on('click', function (e) {
+ var children = $(this).parent('li.parent_li').find(' > ul > li');
+ if (children.is(":visible")) {
+ children.hide('fast');
+ $(this).attr('title', 'Expand this branch').find(' > i').addClass('glyphicon-plus').removeClass('glyphicon-minus');
+ } else {
+ children.show('fast');
+ $(this).attr('title', 'Collapse this branch').find(' > i').addClass('glyphicon-minus').removeClass('glyphicon-plus');
+ }
+ e.stopPropagation();
+ });
+ }
+ function findBy(layer, key, value) {
+ if (layer.get(key) === value) {
+ return layer;
+ }
+ // Find recursively if it is a group
+ if (layer.getLayers) {
+ var layers = layer.getLayers().getArray(),
+ len = layers.length, result;
+ for (var i = 0; i < len; i++) {
+ result = findBy(layers[i], key, value);
+ if (result) {
+ return result;
+ }
+ }
+ }
+ return null;
+ }
+ /*$(document).ready(function () {
+ //console.log("inicio");
+ initializeTree();
+ // Handle opacity slider control
+ $('input.opacity').slideDown().on('slide', function (ev) {
+ var layername = $(this).closest('li').data('layerid');
+ var layer = findBy(map.getLayerGroup(), 'name', layername);
+ layer.setOpacity(ev.value);
+ });
+ // Handle visibility control
+ $('i').on('click', function () {
+ var layername = $(this).closest('li').data('layerid');
+ var layer = findBy(map.getLayerGroup(), 'name', layername);
+ layer.setVisible(!layer.getVisible());
+ if (layer.getVisible()) {
+ $(this).removeClass('glyphicon-unchecked').addClass('glyphicon-check');
+ } else {
+ $(this).removeClass('glyphicon-check').addClass('glyphicon-unchecked');
+ }
+ });
+ });*/
