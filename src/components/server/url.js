982c23d9f7cc7a723a21307560e78da28519e4a5
Camilo Zapata <cazm013@gmail.com>
2022-01-12 18:12:24 -0500

    Change url to geoserver and backend

--- src/components/server/url.js
+++ src/components/server/url.js
@@ -1,6 +1,6 @@
 // all external links and URL's 
-export const GEOSERVER_URL= 'http://34.231.25.67:8080/geoserver/';
+export const GEOSERVER_URL= 'http://44.195.233.148:8080/geoserver/';
 export const GEOSERVER_URL_HUMBOLDT='http://geoservicios.humboldt.org.co/geoserver/';
 
 export const GEONETWORK_URL='http://geonetwork.humboldt.org.co/geonetwork/srv/spa/catalog.search#/metadata/'
-export const PYTHONSERVER='http://ec2-54-197-164-22.compute-1.amazonaws.com:8000/api/'
+export const PYTHONSERVER='http://52.203.40.156:8000/api/'
