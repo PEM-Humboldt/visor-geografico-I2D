14dab695df07ffac8dcbca30e1e03dd48c3de1c0
julian.torres <julian.torres@VN_JTorres_PC.humboldt.local>
2023-08-08 17:29:07 -0500

    cantidad de caracteres formulario de descarga limitado a 50

--- src/components/pageComponent/side-options/tab-charts/create-chart/exportReport/export-modal.js
+++ src/components/pageComponent/side-options/tab-charts/create-chart/exportReport/export-modal.js
@@ -32,22 +32,22 @@ var jsonModal = () => {
       "html": `
             <div class="form-group" id="entidadSolicitante">
               <label for='entitySolicitante'>Entidad del Solicitante*</label>
-              <input type='text' class='form-control' id='entitySolicitante' aria-describedby='entityHelp' placeholder='Ingresar el nombre de la entidad' required>
+              <input type='text'  maxlength="50" class='form-control' id='entitySolicitante' aria-describedby='entityHelp' placeholder='Ingresar el nombre de la entidad' required>
               <div class="invalid-feedback">Por favor ingrese un nombre correcto.</div>
             </div>
             <div class="form-group" id="nombreUser">
               <label for='nameSolicitante'>Nombre del Solicitante*</label>
-              <input type='text' class='form-control' id='nameSolicitante' aria-describedby='nameHelp' placeholder='Ingresar el nombre del solicitante' required>
+              <input type='text'  maxlength="50" class='form-control' id='nameSolicitante' aria-describedby='nameHelp' placeholder='Ingresar el nombre del solicitante' required>
               <div class="invalid-feedback">Por favor ingrese un nombre correcto.</div>
             </div>
             <div class="form-group" id="mailUser">
               <label for='mailSolicitante'>Correo del Solicitante*</label>
-              <input type='email' class='form-control' id='mailSolicitante' aria-describedby='emailHelp' placeholder='Ingresar el correo' required>
+              <input type='email'  maxlength="50" class='form-control' id='mailSolicitante' aria-describedby='emailHelp' placeholder='Ingresar el correo' required>
               <div class="invalid-feedback">Por favor ingrese un correo correcto.</div>
             </div>
             <div class="form-group" id="objetivoUser">
               <label for='objetivoSolicitante'>Objetivo</label>
-              <textarea class='form-control' id='objetivoSolicitante' placeholder='Ingrese la razón por la que descarga la información' rows='1'></textarea>
+              <textarea  maxlength="100" class='form-control' id='objetivoSolicitante' placeholder='Ingrese la razón por la que descarga la información' rows='1'></textarea>
             </div>
 
             <button type='submit' id="downloadPDF" value="downloadPDF" class='btn btn-primary btn-block'>Descargar Informe (PDF)</button>
