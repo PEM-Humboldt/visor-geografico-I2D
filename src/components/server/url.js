e3a8217802e3abe67e95521793c271cecb85833f
julian.torres <julian.torres@VN_JTorres_PC.humboldt.local>
2025-03-17 12:04:32 -0500

    se cambia URL del Back-End, ahora con certificado SSL

--- src/components/server/url.js
+++ src/components/server/url.js
@@ -1,11 +1,12 @@
 // all external links and URL's 
 //export const GEOSERVER_URL= 'http://44.195.233.148:8080/geoserver/';
 export const GEOSERVER_URL= 'https://geoservicios.humboldt.org.co/geoserver/';
-export const GEOSERVER_URL_HUMBOLDT='http://geoservicios.humboldt.org.co/geoserver/';
+//export const GEOSERVER_URL_HUMBOLDT='http://geoservicios.humboldt.org.co/geoserver/';
 
-export const GEONETWORK_URL='http://geonetwork.humboldt.org.co/geonetwork/srv/spa/catalog.search#/metadata/';
+export const GEONETWORK_URL='https://geonetwork.humboldt.org.co/geonetwork/srv/spa/catalog.search#/metadata/';
 export const DATAVERSE_URL='https://doi.org/10.21068/';
 
-export const PYTHONSERVER='http://52.203.40.156:8000/api/';
+export const PYTHONSERVER='https://api-v1s0r.humboldt.org.co/';
+//export const PYTHONSERVER='http://52.203.40.156:8000/api/';
 //export const PYTHONSERVER='http://127.0.0.1:8000/api/'
 //export const PYTHONSERVER='http://localhost:8000/api/'
