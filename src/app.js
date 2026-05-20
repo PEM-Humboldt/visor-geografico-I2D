8823b10c1b27a0dd2a22ccce26e84b9aaad695a2
jlhiguitac <jluis.higuita@gmail.com>
2026-05-20 16:02:09 -0500

    added another start components

---
+++ src/app.js
@@ -0,0 +1,11 @@
+import 'bootstrap'
+// import 'bootstrap/dist/css/bootstrap.css' // Import precompiled Bootstrap css
+// import '@fortawesome/fontawesome-free/css/all.css'
+// import './scss/index.scss'
+import './components/mapComponent/map-control'
+
+import './components/pageComponent/side-bar/side-bar'
+import './components/pageComponent/side-options/side-options'
+import './components/uiLinks'
+
+import './components/tutorialComponent/tutorial'
